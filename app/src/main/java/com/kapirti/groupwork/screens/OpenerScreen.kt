bed07a1cb9322b71d22b837daace14434a7c3975
Hatice Kubra Tekin <53907574+hkubratkn@users.noreply.github.com>
2022-04-19 20:00:32 +0300

    Delete OpenerScreen.kt

--- app/src/main/java/com/kapirti/groupwork/screens/OpenerScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.kapirti.groupwork.screens
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun OpenerScreen(){
-    Text(text="opener screen")
-}
